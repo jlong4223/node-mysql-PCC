e78fb0f0394dcdd56f4f0afa09b5ba1c095e2a74
Jared <jlong4223@gmail.com>
2021-02-16 13:19:01 -0600

    uses home route on server

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
+// imported routes
+const homeRoute = require("./routes/welcome");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger("dev"));
 app.use(cors());
+
+// routes being used
+app.use("/", homeRoute);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
