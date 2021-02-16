47cb27130e71277256cc900af06deacb9346df21
Jared <jlong4223@gmail.com>
2021-02-16 13:11:37 -0600

    successfully connects to mysql db

---
+++ config/db-config.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+dbVars = {
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: process.env.DBPASSWORD,
+  DB: "portchaveriat",
+};
+
+const connection = mysql.createConnection({
+  host: dbVars.HOST,
+  user: dbVars.USER,
+  password: dbVars.PASSWORD,
+  database: dbVars.DB,
+});
+
+connection.connect((error) => {
+  let db = dbVars.DB;
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`Successfully connected to mysql db: ${db}`);
+  }
+});
+
+module.exports = connection;
