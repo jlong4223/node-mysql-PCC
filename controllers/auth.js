960d616ff5a832cbaf2bd33d7c04a4b2b67a5fc4
Jared <jlong4223@gmail.com>
2021-02-17 15:44:29 -0600

    creates login function - NEEDS Tested

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,3 +61,49 @@ exports.register = (req, res) => {
     },
   );
 };
+
+// TODO test the login function
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).render("login", {
+        message: "Please provide email and password",
+      });
+    }
+    db.query(
+      "SELECT * FROM users WHERE email = ?",
+      [email],
+      async (error, results) => {
+        console.log(results);
+        // there should only be one in the database so im using 0
+        if (
+          !results ||
+          //  comparing given password with hashed password
+          !(await bcrypt.compare(password, results[0].password))
+        ) {
+          res.status(401).render("login", {
+            message: "Email or Password is incorrect",
+          });
+        } else {
+          const id = results[0].id;
+          const token = jwt.sign({ id }, process.env.SECRET, {
+            expiresIn: "90d",
+          });
+          console.log("the token is: " + token);
+
+          const cookieOptions = {
+            expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+            httpOnly: true,
+          };
+
+          res.cookie("jwt", token, cookieOptions);
+          res.status(200).redirect("/");
+        }
+      },
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
