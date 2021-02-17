41b816d09dec8fd79c75d90576375f571a80d632
Jared <jlong4223@gmail.com>
2021-02-16 19:47:16 -0600

    requires db, and uses auth routes

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const logger = require("morgan");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
+require("./config/db-config");
 
 // imported routes
 const homeRoute = require("./routes/welcome");
+const authRoute = require("./routes/auth");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +16,7 @@ app.use(cors());
 
 // routes being used
 app.use("/", homeRoute);
+app.use("/", authRoute);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
