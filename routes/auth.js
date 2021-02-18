8a7f3735c652482c54edc9ab07908a76de442d0c
Jared <jlong4223@gmail.com>
2021-02-17 21:44:27 -0600

    creates post route for login

--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const authCtrl = require("../controllers/auth");
 
 router.post("/register", authCtrl.register);
+router.post("/login", authCtrl.login);
 
 module.exports = router;
