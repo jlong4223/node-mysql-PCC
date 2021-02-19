47f8e9a9e5904179b766ed734364cc66844b7b8e
Jared <jlong4223@gmail.com>
2021-02-19 15:19:10 -0600

    removes repeated code

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,8 @@
-const mysql = require("mysql");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const util = require("util");
+const db = require("../config/db-config");
 require("dotenv").config();
-
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: process.env.DBPASSWORD,
-  database: "portchaveriat",
-});
 
 exports.register = (req, res) => {
   const { name, email, password, passwordConfirm } = req.body;
@@ -76,29 +69,27 @@ exports.login = async (req, res) => {
       [email],
       async (error, results) => {
         console.log(results);
-        // there should only be one in the database so im using 0
         if (
           !results ||
-          //  comparing given password with hashed password
+          // there should only be one in the database so im using 0
+          // comparing given password with hashed password
           !(await bcrypt.compare(password, results[0].password))
         ) {
           res.status(401).json({
             message: "Email or Password is incorrect",
           });
         } else {
-          // grabbing the name from the req result
+          // grabbing the name from the req/query result
           const id = results[0].name;
           const token = jwt.sign({ id }, process.env.SECRET, {
             expiresIn: "90d",
           });
-          //   res.json({ token });
           console.log("the token is: " + token + " for " + id);
 
           const cookieOptions = {
             expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
             httpOnly: true,
           };
-          //   res.json({ token, email, password });
           res.cookie("jwt", token, cookieOptions);
           res.status(200).send({
             id,
