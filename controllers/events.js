92bd12b7c45341b15960dab6ef4c9d2e3bb37b7a
Jared Long <jlong4223@gmail.com>
2021-02-28 20:46:15 -0600

    gets all events from mysql db

---
+++ controllers/events.js
@@ -0,0 +1,7 @@
+const db = require("../config/db-config");
+
+exports.getAll = (req, res) => {
+  db.query("SELECT * FROM events", (err, data) => {
+    err ? console.log(err) : res.json(data);
+  });
+};
