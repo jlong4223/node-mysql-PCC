20950ee71058b389b19b07f30c6cd0d7282da24e
Jared Long <jlong4223@gmail.com>
2021-03-01 16:41:06 -0600

    creates post route and tested successfully with postman

--- routes/events.js
+++ routes/events.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const eventsCtrl = require("../controllers/events");
 
 router.get("/events", eventsCtrl.getAll);
+router.post("/events", eventsCtrl.create);
 
 module.exports = router;
